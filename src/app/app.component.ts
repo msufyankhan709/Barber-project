d22a7796bcf190b7abcede7cfa566b6ee610a068
M Sufyan Khan <msufyankhan709@gmail.com>
2021-10-01 18:37:50 +0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
+import {SelectionModel} from '@angular/cdk/collections';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+export interface users {
+  Name: string;
+  Title: string;
+  Company: string;
+  EmailAddress: any;
+  phone:number;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+const ELEMENT_DATA: users[] = [
+  {Name: "Rodney ", Title: 'Hair specialist', Company:"Barber", EmailAddress: "support@barber.com",phone:3026366722},
+  {Name: "Jason", Title: 'Hair specialist', Company:"Barber", EmailAddress: "support@barber.com",phone:3026366722} ,
+  {Name: "Nelson", Title: 'Hair specialist', Company:"Barber", EmailAddress: "support@barber.com",phone:3026366722} ,
+  {Name: "Inverness", Title: 'Hair specialist', Company:"Barber", EmailAddress: "support@barber.com",phone:3026366722}  ,
+  {Name: "Fergus", Title: 'Hair specialist', Company:"Barber", EmailAddress: "support@barber.com",phone:3026366722} , 
+  {Name: "Ement", Title: 'Hair specialist', Company:"Barber", EmailAddress: "support@barber.com",phone:3026366722},
+  {Name: "Peppertrout", Title: 'Hair specialist', Company:"Barber", EmailAddress: "support@barber.com",phone:3026366722},
+  {Name: "Niles", Title: 'Hair specialist', Company:"Barber", EmailAddress: "support@barber.com",phone:3026366722},
+  {Name: "Stieve", Title: 'Hair specialist', Company:"Barber", EmailAddress: "support@barber.com",phone:3026366722},
 ];
 
 
@@ -31,9 +34,9 @@ const ELEMENT_DATA: PeriodicElement[] = [
 export class AppComponent {
   title = 'BarberProject';
 
-  displayedColumns: string[] = ['select', 'position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-  selection = new SelectionModel<PeriodicElement>(true, []);
+  displayedColumns: string[] = ['select','Name','Title','Company','EmailAddress','phone'];
+  dataSource = new MatTableDataSource<users>(ELEMENT_DATA);
+  selection = new SelectionModel<users>(true, []);
 
   /** Whether the number of selected elements matches the total number of rows. */
   isAllSelected() {
@@ -53,10 +56,10 @@ export class AppComponent {
   }
 
   /** The label for the checkbox on the passed row */
-  checkboxLabel(row?: PeriodicElement): string {
+  checkboxLabel(row?: users): string {
     if (!row) {
       return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
     }
-    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.Name+ 1}`;
   }
 }
